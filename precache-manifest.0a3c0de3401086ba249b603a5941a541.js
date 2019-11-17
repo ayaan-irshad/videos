dba0dd9e04fb520cee59cdfe7ebf8dbfb8938bfb
ayaan-irshad <ayaanirshadbhat@gmail.com>
2019-11-17 23:31:12 +0530

    Updates

---
+++ precache-manifest.0a3c0de3401086ba249b603a5941a541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308849874f3f0b05e4ef7a0782cae43c",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "e96fdcb1be8b64ffcf45",
+    "url": "/videos/static/css/main.8b8ded97.chunk.css"
+  },
+  {
+    "revision": "4b77446d1d38f77ff332",
+    "url": "/videos/static/js/2.636f9ff4.chunk.js"
+  },
+  {
+    "revision": "e96fdcb1be8b64ffcf45",
+    "url": "/videos/static/js/main.010f155e.chunk.js"
+  },
+  {
+    "revision": "c1412fb9867b9092fb2d",
+    "url": "/videos/static/js/runtime-main.12b1e5ae.js"
+  }
+]);
